e23eb7a8ec96041959eb6a9a16d3d98b9e7f43d8
Ricardo <ricardo@paxlife.aero>
2017-05-07 10:51:36 +0200

    Final ngRout typewrite ngAnimate

--- src/js/manifest.js
+++ src/js/manifest.js
@@ -1,3 +1,7 @@
 
-//= include angular.min.js
- 
+//= include inc/angular.min.js
+//= include inc/angular-route.js
+//= include inc/angular-animate.min.js
+
+//= include app.js
+//= include inc/typewriter.js
